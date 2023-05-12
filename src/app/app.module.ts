9c3cd44f8aa280ef3988ad2628e62d7b0259e5cf
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-05-12 00:02:22 -0300

    firs asignated to code in html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,7 @@ import { Ejercicio3Component } from './ejercicio3/ejercicio3.component';
 @NgModule({
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, FormsModule
   ],
   declarations: [
     AppComponent,
--- src/app/ejercicio1/ejercicio1.component.ts
+++ src/app/ejercicio1/ejercicio1.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./ejercicio1.component.css']
 })
 export class Ejercicio1Component {
+  codigo: string = '';
+  feedback: string = '';
 
+  verificar() {
+    const respuesta = this.codigo;
+    const feedback = document.createElement('div');
+
+    try {
+      // Aquí creamos una función anónima con el código proporcionado por el usuario
+      const usuarioFunc = new Function("a", "b", respuesta);
+
+      // Verificamos si la función creada por el usuario devuelve el resultado correcto
+      if (usuarioFunc(2, 3) === 5) {
+        feedback.innerHTML = "¡Respuesta correcta!";
+      } else {
+        feedback.innerHTML = "Respuesta incorrecta, vuelve a intentarlo.";
+      }
+    } catch (error) {
+      // Si hay un error en la evaluación del código proporcionado por el usuario, indicamos que la respuesta es incorrecta
+      feedback.innerHTML = "Respuesta incorrecta, vuelve a intentarlo.";
+    }
+
+    this.feedback = feedback.innerHTML;
+  }
 }
